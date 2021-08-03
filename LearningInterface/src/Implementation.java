a55c274f4fdaf79628d9b38d8d117dd405ef86e0
Kelly Click <kclick91@gmail.com>
2021-08-03 12:52:14 -0400

    Add files via upload

---
+++ LearningInterface/src/Implementation.java
@@ -0,0 +1,196 @@
+public class Implementation implements ILearning {
+    @Override
+    public void Store() {
+
+    }
+
+    @Override
+    public void Synthesize(Object o, Object otwo) {
+
+    }
+
+    @Override
+    public void Decompose(Object o) {
+
+    }
+
+    @Override
+    public void Link(Object O, Object o) {
+
+    }
+
+    @Override
+    public void Categorize(Object o) {
+
+    }
+
+    @Override
+    public void Detect() {
+
+    }
+
+    @Override
+    public void Detect(Object o) {
+
+    }
+
+    @Override
+    public void UnderstandConsequences() {
+
+    }
+
+    @Override
+    public String ExplainEvents() {
+        return null;
+    }
+
+    @Override
+    public String ExplainItself() {
+        return null;
+    }
+
+    @Override
+    public void ImproveSelf() {
+
+    }
+
+    @Override
+    public void Calculate() {
+
+    }
+
+    @Override
+    public void Train() {
+
+    }
+
+    @Override
+    public void Train(Object o) {
+
+    }
+
+    @Override
+    public void FindSimilarities(Object o, Object otwo) {
+
+    }
+
+    @Override
+    public void FindDifferences(Object o, Object otwo) {
+
+    }
+
+    @Override
+    public String DefendDecision(Object o) {
+        return null;
+    }
+
+    @Override
+    public void UseIntuition() {
+
+    }
+
+    @Override
+    public void Imagine() {
+
+    }
+
+    @Override
+    public void Teach() {
+
+    }
+
+    @Override
+    public String FindInsight() {
+        return null;
+    }
+
+    @Override
+    public String FindBias() {
+        return null;
+    }
+
+    @Override
+    public String FindFallacy() {
+        return null;
+    }
+
+    @Override
+    public void SeekReward() {
+
+    }
+
+    @Override
+    public void AvoidPunishment() {
+
+    }
+
+    @Override
+    public Object Translate(Object o) {
+        return null;
+    }
+
+    @Override
+    public String Name() {
+        return null;
+    }
+
+    @Override
+    public int Rate() {
+        return 0;
+    }
+
+    @Override
+    public String Conclude(String s) {
+        return null;
+    }
+
+    @Override
+    public String TransferKnowledge() {
+        return null;
+    }
+
+    @Override
+    public void PayAttentionTo(Object o) {
+
+    }
+
+    @Override
+    public void Understand(Object o) {
+
+    }
+
+    @Override
+    public void Strategize() {
+
+    }
+
+    @Override
+    public void UseTactic(Object tactic) {
+
+    }
+
+    @Override
+    public String WriteAlgorithm() {
+        return null;
+    }
+
+    @Override
+    public Boolean DoesUnderstand(Object o) {
+        return null;
+    }
+
+    @Override
+    public String GiveBigPicture() {
+        return null;
+    }
+
+    @Override
+    public Boolean ExpressABelief(String s) {
+        return null;
+    }
+
+    @Override
+    public int WeighOptions(Object firstOption, Object secondOption) {
+        return 0;
+    }
+}
